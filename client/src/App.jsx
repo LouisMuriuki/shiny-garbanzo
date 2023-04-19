08595febbd1165269fcd74bdbf66fc4047e1ea38
Louis <luihugo247@gmail.com>
2023-04-19 18:04:53 +0300

    first comit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {logo} from "./assets"
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Hello world</h1>
+    </div>
+  );
+};
+
+export default App;

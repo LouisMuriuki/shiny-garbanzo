f06ed3bb7ec29a8c1294bf4f8d40676ae563d078
Louis <luihugo247@gmail.com>
2023-05-10 20:56:42 +0300

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
 
   const GetPosts = useCallback(async () => {
     const res = await axios.get(
-      `http://localhost:8080/api/v1/post?page=${page}&limit=${limit}`,
+      `https://dalle-qgms.onrender.com/api/v1/post?page=${page}&limit=${limit}`,
       {
         method: "GET",
         headers: {
